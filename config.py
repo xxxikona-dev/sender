039797f84c4d29e538db97027b2cd6811eac7d1e
xxxikona-dev <xxxikona@gmail.com>
2026-05-30 14:21:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 # Токен главного Telegram-бота из @BotFather
-BOT_TOKEN = "ТВОЙ_ТОКЕН_БОТА"
+BOT_TOKEN = "8685634422:AAEu4v0GJ61WL_MudkoB-2K1cfgegYX6puE"
 
 # Данные API (получать на my.telegram.org)
-API_ID = 1234567  # Должно быть числом (int)
-API_HASH = "ТВОЙ_API_HASH"
+API_ID = 29797368  # Должно быть числом (int)
+API_HASH = "5d37c3b7bf4bc792eba9d619aaaa9870"
 
 # Название файла базы данных SQLite
 DB_NAME = "bot_data.db"
