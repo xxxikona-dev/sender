e250b85d8de63ae790f06fab5b77ac3defc8ed4e
xxxikona-dev <xxxikona@gmail.com>
2026-05-30 14:13:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Токен главного Telegram-бота из @BotFather
+BOT_TOKEN = "ТВОЙ_ТОКЕН_БОТА"
+
+# Данные API (получать на my.telegram.org)
+API_ID = 1234567  # Должно быть числом (int)
+API_HASH = "ТВОЙ_API_HASH"
+
+# Название файла базы данных SQLite
+DB_NAME = "bot_data.db"
